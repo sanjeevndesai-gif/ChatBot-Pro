8a7b9c8d377179b0cff270836910923492a72026
sanjeevndesai-gif <sanjeev.n.desai@gmail.com>
2026-07-23 12:50:10 +0530

    dev: load @angular/compiler for dev JIT compilation

--- frontend/chat_bot/src/main.ts
+++ frontend/chat_bot/src/main.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import '@angular/compiler';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
 import { ConfigService } from './app/services/config.service';
