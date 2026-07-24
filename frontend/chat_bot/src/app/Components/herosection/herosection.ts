5468a80aff0481da62ebd705ff344732f9d04b70
sanjeevndesai-gif <sanjeev.n.desai@gmail.com>
2026-07-24 17:21:41 +0530

    herosection: use programmatic navigation to /register (button click)

--- frontend/chat_bot/src/app/Components/herosection/herosection.ts
+++ frontend/chat_bot/src/app/Components/herosection/herosection.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 // import { TranslatePipe } from '../../pipes/translate.pipe';
 // import { I18nService } from '../../services/i18n.service';
@@ -11,6 +12,12 @@ import { RouterModule } from '@angular/router';
   styleUrls: ['./herosection.scss']
 })
 export class Herosection {
+
+  constructor(private router: Router) {}
+
+  navigateToRegister(): void {
+    this.router.navigate(['/register']);
+  }
 
   // constructor(public i18n: I18nService) { }
 
